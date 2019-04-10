4a91bd647d3a385843da091d743f99108ba7d006
Brandon Allison <brandona8128@outlook.com>
2019-04-09 22:35:38 -0400

    added depedencies/package/actions(creator)

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -6,6 +6,7 @@ const initialState = {
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
     // Fill me in with the important reducers
+    
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
     default:
